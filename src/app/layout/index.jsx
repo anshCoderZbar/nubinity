5ced86807b5d94e58952006904e77af9e0cc666a
coderzbar <coderzbar@gmail.com>
2023-12-05 16:31:44 +0530

    working with nubinity changes

--- src/app/layout/index.jsx
+++ src/app/layout/index.jsx
@@ -3,32 +3,13 @@ import { Header } from "./header";
 import { Footer } from "./footer";
 
 import "styles/Layout.css";
-import { useLocation } from "react-router-dom";
-import { LoadingRoute } from "components/pageloading";
 
 export const Layout = ({ children }) => {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    setLoading(true);
-    const timeout = setTimeout(() => {
-      setLoading(false);
-    }, 500);
-
-    return () => clearTimeout(timeout);
-  }, []);
-
   return (
-    <>
-      {loading ? (
-        <LoadingRoute />
-      ) : (
-        <React.Fragment>
-          <Header />
-          {children}
-          <Footer />
-        </React.Fragment>
-      )}
-    </>
+    <React.Fragment>
+      <Header />
+      {children}
+      <Footer />
+    </React.Fragment>
   );
 };
