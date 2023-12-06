39c7c34cc1307f18d0a9780f3b631eda7070cf94
coderzbar <coderzbar@gmail.com>
2023-12-06 11:07:00 +0530

    added chatwoot

--- src/app/layout/footer/index.jsx
+++ src/app/layout/footer/index.jsx
@@ -14,6 +14,35 @@ export const Footer = () => {
     }, 2500);
   }, []);
 
+  console.log(process.env.REACT_APP_CHAT_WIDGET_WEBSITETOKEN);
+
+  useEffect(() => {
+    const BASE_URL = "https://app.chatwoot.com";
+    const script = document.createElement("script");
+
+    const initializeChatwoot = () => {
+      if (window.chatwootSDK) {
+        window.chatwootSDK.run({
+          websiteToken: process.env.REACT_APP_CHAT_WIDGET_WEBSITETOKEN,
+          baseUrl: BASE_URL,
+        });
+      } else {
+        console.error("Chatwoot SDK not loaded.");
+      }
+    };
+
+    script.src = BASE_URL + "/packs/js/sdk.js";
+    script.defer = true;
+    script.async = true;
+
+    script.onload = initializeChatwoot;
+    script.onerror = () => {
+      console.error("Error loading Chatwoot SDK.");
+    };
+
+    document.body.appendChild(script);
+  }, []);
+
   return (
     <footer className="footer">
       {cookieNoti && (
